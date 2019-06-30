0229909c85b0a10a503a084aa01940a2785fdd2a
ShivamBhanvadia <33751996+ShivamBhanvadia@users.noreply.github.com>
2019-06-30 18:38:30 +0530

    Initial commit

---
+++ twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "1027222580996919296-bGmYagHOJbVcPG5Tfs5fnlosM6rxEW"
+ACCESS_TOKEN_SECRET = "fetWjAWbUp8xZWVqIwgPzzpHKel8TrpR8x7rImy8ShLm3"
+CONSUMER_KEY = "hc6kUzikRLNZKtNZhtLhIO64F"
+CONSUMER_SECRET = "Z0rjeoq5so1tIcH8hWjTjdfUaOOp13KrHlLcxHXRWNiYXYudSC"
