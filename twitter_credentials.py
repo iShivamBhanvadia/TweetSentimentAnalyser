ffec514d7b730644e03c57bce25d507fe2f9e2ae
Shivam Bhanvadia <52375275+iShivamBhanvadia@users.noreply.github.com>
2021-04-05 11:22:55 +0530

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,5 +1,5 @@
 # Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = "1027222580996919296-bGmYagHOJbVcPG5Tfs5fnlosM6rxEW"
-ACCESS_TOKEN_SECRET = "fetWjAWbUp8xZWVqIwgPzzpHKel8TrpR8x7rImy8ShLm3"
-CONSUMER_KEY = "hc6kUzikRLNZKtNZhtLhIO64F"
-CONSUMER_SECRET = "Z0rjeoq5so1tIcH8hWjTjdfUaOOp13KrHlLcxHXRWNiYXYudSC"
+ACCESS_TOKEN = "xxxx"
+ACCESS_TOKEN_SECRET = "xxxx"
+CONSUMER_KEY = "xxxx"
+CONSUMER_SECRET = "xxxx"
